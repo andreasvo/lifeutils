ac16340f195bfd473c14949f0caf77c931fa6400
avo <avo1993@live.no>
2022-01-14 19:26:55 +0100

    Add spring boot

---
+++ lifeutils/src/main/kotlin/no/avo/lifeutils/LifeutilsApplication.kt
@@ -0,0 +1,11 @@
+package no.avo.lifeutils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LifeutilsApplication
+
+fun main(args: Array<String>) {
+	runApplication<LifeutilsApplication>(*args)
+}
---
+++ lifeutils/src/test/kotlin/no/avo/lifeutils/LifeutilsApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.avo.lifeutils
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LifeutilsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
